bc22e59f55de4236ce7ff05eed3ac7b150b558d6
bautibonasso-arch <bautibonasso@gmail.com>
2025-08-11 12:24:40 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    suma=a+b
+    resta=a-b
+    multiplicacion=a*b
+    promedio=suma/2
+    resto=a%b
+    division=round(a/b)
+    division1=a/b
+    print(suma)
+    print(resta)
+    print(multiplicacion)
+    print(promedio)
+    print(division)
+    print(resto)
+    print(division1)
+math()
